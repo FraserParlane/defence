744ff2c27502c93d06fb98836b2551bff52929bc
Fraser Parlane <mail@fraserparlane.com>
2022-08-05 09:37:41 -0700

    Figure created.

--- ordered_pareto_front/ordered_apareto_front.py
+++ ordered_pareto_front/ordered_apareto_front.py
@@ -23,6 +23,38 @@ def read_data():
     return df
 
 
+def plot_data():
+    """
+    Plot the processed campaign data in order of sampling.
+    :return: None
+    """
+
+    # Create the plotting objects
+    df = read_data()
+    n_campaigns = len(df['campaign'].unique())
+    figure: plt.Figure = plt.figure(figsize=(10, 6))
+    axes = [[], []]
+    for i in range(2):
+        for j in range(n_campaigns):
+            ax = figure.add_subplot(2, n_campaigns, i * n_campaigns + j + 1)
+            axes[i].append(ax)
+
+    # Format figure
+    figure.subplots_adjust(
+        left=0.05,
+        right=0.95,
+        bottom=0.05,
+        top=0.95,
+        wspace=0.05,
+        hspace=0.05,
+    )
+
+    # Save
+    name = os.path.basename(__file__).split('.')[0]
+    figure.set_dpi(300)
+    figure.savefig(f'{name}.png')
+    figure.savefig(f'{name}.svg')
+
+
 if __name__ == '__main__':
-    data = read_data()
-    print(data)
+    plot_data()
