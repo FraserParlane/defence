a6701976b8096693acbbc85d91b974da184f4a3f
Fraser Parlane <mail@fraserparlane.com>
2022-08-06 21:12:49 -0700

    Skeleton of classes for steel.

--- steel/steel.py
+++ steel/steel.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from typing import List
 import pandas as pd
 import numpy as np
+import os
 
 # A plot of the complexity of steel over time. See:
 # https://docs.google.com/document/d/1ZkReceEGZomda2GGzVP1f2HIrID8KVW9nJ3OUeg8woA/edit
@@ -10,15 +11,14 @@ import numpy as np
 # Define the compositions of the steels
 class Element:
     """
-    One component of a steel.
+    One element of a steel.
     """
     def __init__(
             self,
-            name: str,
             frac: float,
             color: str,
     ):
-        self.name = name
+        self.name = self.__class__.__name__
         self.frac = frac
         self.color = color
 
@@ -32,14 +32,14 @@ class Steel:
     """
     def __init__(
             self,
-            name: str,
+            elements: List[Element],
             year: int,
-            components: List[Element]
+            name: str = 'None',
     ):
         self.name = name
         self.year = year
-        self.components = sorted(components)
-        self.count = len(self.components)
+        self.elements = sorted(elements)
+        self.count = len(self.elements)
 
     def __lt__(self, other):
         return self.year < other.year
@@ -60,7 +60,6 @@ class Steels:
 class C(Element):
     def __init__(self, frac: float):
         super().__init__(
-            name='C',
             frac=frac,
             color='#555555',
         )
@@ -69,19 +68,52 @@ class C(Element):
 class Cr(Element):
     def __init__(self, frac: float):
         super().__init__(
-            name='Cr',
             frac=frac,
-            color='#123456',
+            color='#333333',
+        )
+
+
+class Ni(Element):
+    def __init__(self, frac: float):
+        super().__init__(
+            frac=frac,
+            color='#777777',
         )
 
 
 # Define the steels
 steel = Steels(
     steels=[
+        Steel(
+            year=1865,
+            elements=[
+                C(frac=1),
+                Cr(frac=2),
+            ]
+        ),
+        Steel(
+            year=1888,
+            elements=[
+                C(frac=1),
 
+            ]
+        )
     ]
 )
 
 
+def run():
+    """
+    Plot the steel data.
+    :return: None
+    """
+
+    # Create the plot
+    figure: plt.Figure = plt.figure()
+    ax: plt.Axes = figure.add_subplot()
+
+    # Save
+
+
 if __name__ == '__main__':
-    print(steel)
+    run()
